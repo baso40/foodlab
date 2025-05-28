82ab6b5092b6c5f258ddfea015d3448de9abf667
baso40 <nishit.kiran36@gmail.com>
2025-05-29 02:05:09 +0530

    category and restaurant entity

--- foodlab/src/main/java/com/example/foodlab/entity/Dish.java
+++ foodlab/src/main/java/com/example/foodlab/entity/Dish.java
@@ -1,5 +1,41 @@
 package com.example.foodlab.entity;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "dish")
 public class Dish {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String name;
+	
+	private String description;
+	
+	@DecimalMin(value = "0.0", inclusive = false)
+	private BigDecimal price;
+	
+	private Integer stockQuantity;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category Category;
+	
+	@ManyToOne
+	@JoinColumn(name = "restraunt_id")
+	private Restaurant restraunt;
 
 }
